8cb63dd16ea64f718e1ffcce7a302c98253a6037
Orxanbbyv <bbyvorxan13@gmail.com>
2023-02-25 05:19:48 -0800

    lesson

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import Nav from "./components/Nav"
+import "./styles/reset.scss"
+import "./styles/global.scss"
+import "./assets/app.scss"
+function App() {
+
+  return (
+    <>
+   <Nav/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Nav/index.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import "./nav.scss"
+const Nav = () => {
+    return (
+        <nav>
+            <div className="container">
+
+                <div className="nav-left">
+
+                    <h1>MORENT</h1>
+                    <div className="inpt-c">
+
+                    </div>
+                </div>
+            </div>
+        </nav>
+    )
+}
+
+export default Nav
