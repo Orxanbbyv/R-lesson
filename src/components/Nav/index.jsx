54a6986df39463661e1c966af8a208fdf476039a
Orxanbbyv <bbyvorxan13@gmail.com>
2023-02-25 06:02:57 -0800

    lesson

--- src/components/Nav/index.jsx
+++ src/components/Nav/index.jsx
@@ -1,4 +1,9 @@
 import React from 'react'
+import Filter from "../../assets/icon/filter.svg"
+import Search from "../../assets/icon/search-normal.svg"
+import Like from "../../assets/icon/like.svg"
+import Nofication from "../../assets/icon/notification.svg"
+import Settings from "../../assets/icon/settings.svg"
 import "./nav.scss"
 const Nav = () => {
     return (
@@ -9,8 +14,16 @@ const Nav = () => {
 
                     <h1>MORENT</h1>
                     <div className="inpt-c">
-
+                        <img src={Search} alt="logo" />
+                        <input type="text" placeholder='Search something here ' />
+                        <img src={Filter} alt="logo" />
                     </div>
+                </div>
+                <div className="nav-right">
+                    <span><img src={Like} alt="logo" /></span>
+                    <span><img src={Nofication} alt="logo" /></span>
+                    <span><img src={Settings} alt="logo" /></span>
+                    <span></span>
                 </div>
             </div>
         </nav>
